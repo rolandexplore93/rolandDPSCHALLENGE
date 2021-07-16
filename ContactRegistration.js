72d36b664c4c55541f2310e3382d7d34425eeece
rolandexplore93 <roland2rule@gmail.com>
2021-07-16 11:45:12 +0100

    //Added a command to the SAVE button to receive user info when they fill the registration

---
+++ ContactRegistration.js
@@ -0,0 +1,10 @@
+//Add a command to the SAVE button to receive user info when they fill the registration
+document.getElementById('saveit').addEventListener('click', ()=>{
+    let fname = document.getElementById('firstname').value;
+			let lname = document.getElementById('lastname').value;
+			let email = document.getElementById('email').value;
+			let phone = document.getElementById('phone').value;
+			let job = document.getElementById('job').value;
+
+
+})
