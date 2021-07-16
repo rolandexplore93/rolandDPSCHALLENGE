ee9b1fb41b63d60c3cd22e90b8b83d1c97c70a9b
rolandexplore93 <roland2rule@gmail.com>
2021-07-16 13:34:20 +0100

    Get exact user ID key on clicking user profile from Index home page

---
+++ ContactView.js
@@ -0,0 +1,15 @@
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+
+    //redirect id from the url
+    // NOTE: Each user profile has its key identity
+var redirectid = findGetParameter('redirectid');
+    //contact list from localstorage
